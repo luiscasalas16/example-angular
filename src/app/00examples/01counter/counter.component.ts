30e6f289c142dc18f49b4aa6de0b8647b68861b0
Luis Carlos Salas Villalobos <lusalas16@gmail.com>
2024-06-27 18:36:44 -0600

    ejemplo

--- src/app/00examples/01counter/counter.component.ts
+++ src/app/00examples/01counter/counter.component.ts
@@ -2,31 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: `
-    <div class="row justify-content-center text-center">
-      <div class="col-auto">
-        <h2>Examples / Counter</h2>
-
-        <hr />
-
-        <h2>{{ tittle }}</h2>
-
-        <h4>
-          Base: <strong> {{ base }} </strong>
-        </h4>
-
-        <button type="button" class="btn btn-primary" (click)="decrease(base)">
-          - {{ base }}
-        </button>
-
-        <span> {{ total }} </span>
-
-        <button type="button" class="btn btn-primary" (click)="increase(base)">
-          + {{ base }}
-        </button>
-      </div>
-    </div>
-  `,
+  templateUrl: './counter.component.html',
 })
 export class CounterComponent {
   tittle: string = 'Contador App';
